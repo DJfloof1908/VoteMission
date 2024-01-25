4755c0c5709e68a9d21cc73ed79345f217c39e34
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-25 06:58:02 +0100

    added config

--- src/main/java/com/kryeit/votemission/missions_types/VoteMission.java
+++ src/main/java/com/kryeit/votemission/missions_types/VoteMission.java
@@ -1,5 +1,6 @@
 package com.kryeit.votemission.missions_types;
 
+import com.kryeit.Main;
 import com.kryeit.missions.MissionDifficulty;
 import com.kryeit.missions.MissionManager;
 import com.kryeit.missions.MissionType;
@@ -15,7 +16,7 @@ import java.util.UUID;
 import static com.kryeit.votemission.Main.ID;
 
 public class VoteMission implements MissionType {
-	private static final Identifier IDENTIFIER = new Identifier(ID, "votes");
+	private static final Identifier IDENTIFIER = new Identifier(Main.MOD_ID, "votes");
 
 	public static void handleVote(UUID player) {
 		MissionManager.incrementMission(player, VoteMission.class, IDENTIFIER, 1);
